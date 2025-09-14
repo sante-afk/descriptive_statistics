752646aafad1de98fd6e1d099aa0413621fdf58f
sante <86688897+sshyta@users.noreply.github.com>
2025-09-14 15:26:18 +0300

    add: task description

--- task1.R
+++ task1.R
@@ -1,3 +1,6 @@
+# Task 1. Create a vector consisting of 10 integer numbers. 
+# Calculate the sum of all elements in the vector that are greater than 5 but less than 10. 
+# Store the sum in a variable named my_sum.
 vec <- 1:10
 result_func <- function(vec) {
   filtered_vec <- vec[vec > 5 & vec < 10]
